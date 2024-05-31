1040b9884819408d010ebc8f5bd1552ae1a37294
el0ooo <eloichayer@gmail.com>
2024-05-31 13:56:39 -0400

    mise en place des boutons pour selection des pays

--- pays.php
+++ pays.php
@@ -5,7 +5,7 @@
  */
 /*
 Plugin name: Pays
-Plugin uri: 
+Plugin uri: https://github.com/el0ooo/EF_2024_4W4
 Version: 1.0.0
 Description: Permet d'afficher les destinations selon le pays choisit
 */
@@ -28,6 +28,23 @@ add_action('wp_enqueue_scripts', 'ec_pays_enqueue');
 
 // cration des destionations associées à un pays
 function creation_destinations(){
-    
+    $contenu = '<div class="contenu__restapi__pays">
+        <button class="cat_bouton" id="France">France</button>
+        <button class="cat_bouton" id="États-Unis">États-Unis</button>
+        <button class="cat_bouton" id="Canada">Canada</button>
+        <button class="cat_bouton" id="Chili">Chili</button>
+        <button class="cat_bouton" id="Argentine">Argentine</button>
+        <button class="cat_bouton" id="Belgique">Belgique</button>
+        <button class="cat_bouton" id="Maroc">Maroc</button>
+        <button class="cat_bouton" id="Mexique">Mexique</button>
+        <button class="cat_bouton" id="Espagne">Japon</button>
+        <button class="cat_bouton" id="Italie">Italie</button>
+        <button class="cat_bouton" id="Islande">Islande</button>
+        <button class="cat_bouton" id="Chine">Chine</button>
+        <button class="cat_bouton" id="Grèce">Grèce</button>
+        <button class="cat_bouton" id="Suisse">Suisse</button>
+    </div>';
 }
+
+add_shortcode('EC_pays', 'creation_destinations');
 ?>
